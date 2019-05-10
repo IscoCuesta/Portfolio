0f26454efbb669f9cc891c596139f50a507b0df6
Isco <Iscocuesta@hotmail.com>
2019-05-10 14:06:55 -0500

    first page working

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+var materialIcons = [
+    "account_balance", "assignment_ind", "assignment", "brightness_7", "brightness_5", "business_center", "contact_mail", "burst_mode"
+
+];
+
+$('.dropdown-trigger').dropdown();
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+
+(function($) {
+    $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
+        {               // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
+        onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
+
+        }
+    });
+}(jQuery));
